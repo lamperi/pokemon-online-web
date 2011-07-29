9740121c7ce51709a5459e84451b9844520ba8c2
coyotte508 <eliott.coyac@gmail.com>
2011-07-29 23:39:35 +0200

    other fixes mentioned in the dev thread

--- js/ui.js
+++ js/ui.js
@@ -312,7 +312,7 @@ UI = function() {
                 $chatdisplay = $(".chatdisplay", pm_dialog);
                 $chatdisplay.append(fancyName(userColour(me), me.name) + htmlEscape(message) + "<br>");
                 /* Resizes correctly */
-                pm_dialog.resize();
+                pm_dialog.dialog("resize");
                 /* Scrolls to the top */
                 var plainElement = $chatdisplay[0];
                 plainElement.scrollTop = plainElement.scrollHeight;
@@ -386,7 +386,7 @@ UI = function() {
         var h = chat.height();
         chat.append(message + "<br>");
         chat.height(h);
-        $(chat).attr({ scrollTop: $(chat).attr("scrollHeight") });
+        $(chat).each(function() {this.attr({ scrollTop: this.attr("scrollHeight") })});
     }
 
     var getColour = function(user, name) {
