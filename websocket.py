d0839bd27b5f6284e9a3310aa3378cca8f1fb1bc
coyotte508 <eliott.coyac@gmail.com>
2011-08-02 23:20:48 +0200

    Fixed websocket crash

--- websocket.py
+++ websocket.py
@@ -465,6 +465,8 @@ class WebSocketFrameDecoder(object):
                 self._currentFrameLength = 0
                 frame = "".join(self._data) + data[:endIndex]
                 self._data[:] = []
+                if len(data) == 0:
+                    break;
                 if frame[0] != "\x00":
                     self.request.transport.loseConnection()
                     break
